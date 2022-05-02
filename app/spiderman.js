90480fdaf9c667c22f806c2f151594a2d1fb21d0
PaolaDaft <andrepaolagd@gmail.com>
2022-05-01 19:19:13 -0500

    Agregando la clase spiderman

---
+++ app/spiderman.js
@@ -0,0 +1,12 @@
+class Spiderman {
+    constructor(name, age, actor, movies, studio) {
+      this.name = name
+      this.age = age
+      this.actor = actor
+      this.movies = movies
+      this.studio = studio
+    }
+  }
+  
+  // Esta línea nos permite exportar nuestra clase
+  module.exports = Spiderman
