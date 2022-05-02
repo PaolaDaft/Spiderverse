b96bc6be1816b793ea426d1d096ba3655520d71a
PaolaDaft <andrepaolagd@gmail.com>
2022-05-01 20:58:40 -0500

    test para method get Info()

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -10,3 +10,8 @@ describe("Unit Tests for Spiderman Class", () => {
         expect(andrewGarfield.studio).toBe("Sony");
     });
 })
+
+test("2) Use the method get Info()", () => {
+    const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
+    expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
+});
